ccab5eaf5281bf19fd9ea90ea7c13745b1347bca
Jakedalus <jacquedalus@gmail.com>
2021-05-08 17:52:28 -0400

    Add various components, start laying out Switch and Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffecte } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import SingleUserPage from './components/SingleUserPage';
+import UsersPage from './components/UsersPage';
+import CoursesPage from './components/CoursesPage';
+import SingleCoursePage from './components/SingleCoursePage';
+import AdminPanel from './components/AdminPanel';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>Carna</h1>
+			<div>
+				<h2>Welcome, {'jake'}</h2>
+				<Switch>
+					<Route path='/users/:id'>
+						<SingleUserPage />
+					</Route>
+					<Route path='/users'>
+						<UsersPage />
+					</Route>
+					<Route path='/courses/:id'>
+						<SingleCoursePage />
+					</Route>
+					<Route path='/courses'>
+						<CoursesPage />
+					</Route>
+					<Route path='/'>
+						<AdminPanel />
+					</Route>
+				</Switch>
+			</div>
+		</div>
+	);
 }
 
 export default App;
