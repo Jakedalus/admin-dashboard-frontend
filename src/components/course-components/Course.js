ed409b579bdd696a929db60d3265ab7fe487242c
Jakedalus <jacquedalus@gmail.com>
2021-05-10 16:15:35 -0400

    List rest of course info in Course component

--- src/components/course-components/Course.js
+++ src/components/course-components/Course.js
@@ -4,6 +4,16 @@ const Course = ({ course }) => {
 	return (
 		<li>
 			<h3>{course.title}</h3>
+			<p>{course.teacher}</p>
+			<p>{course.subject}</p>
+			<ul>
+				{course.questions.map(q => (
+					<li>
+						<p>Question: {q.question}</p>
+						<p>Answer: {q.answer}</p>
+					</li>
+				))}
+			</ul>
 		</li>
 	);
 };
