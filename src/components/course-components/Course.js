55227ed7482481770467018e939feafabc64806b
Jakedalus <jacquedalus@gmail.com>
2021-05-10 18:12:57 -0400

    Clean up Course component to just list the basic info, Link to the SingleCoursePage with more info and delete/edit functionality

--- src/components/course-components/Course.js
+++ src/components/course-components/Course.js
@@ -1,72 +1,19 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+
 import { Link } from 'react-router-dom';
-import { deleteCourse } from '../../reducers/courseReducer';
-import CourseForm from './CourseForm';
 
-const Course = ({ course, admin }) => {
-	const dispatch = useDispatch();
-
-	const [ editMode, setEditMode ] = useState(false);
-
-	console.log(`admin`, admin);
+const Course = ({ course }) => {
 	console.log(`course.user`, course.user);
-	console.log(
-		`admin.id === course.user.id `,
-		admin.id === course.user.id
-	);
-
-	const headers = {
-		headers : { Authorization: `bearer ${admin.token}` }
-	};
-
-	const handleDeleteCourse = id => {
-		dispatch(deleteCourse(id, headers));
-	};
 
 	return (
 		<li>
-			{!editMode && (
-				<div>
-					<Link to={`/courses/${course.id}`}>
-						<h3>{course.title}</h3>
-					</Link>
-					<p>{course.teacher}</p>
-					<p>{course.subject}</p>
-					<ul>
-						{course.questions.map(q => (
-							<li key={q._id}>
-								<p>Question: {q.question}</p>
-								<p>Answer: {q.answer}</p>
-							</li>
-						))}
-					</ul>
-					{admin.id === course.user.id && (
-						<button onClick={() => setEditMode(true)}>
-							Edit
-						</button>
-					)}
-					{admin.id === course.user.id && (
-						<button
-							onClick={() => handleDeleteCourse(course.id)}
-						>
-							Delete
-						</button>
-					)}
-				</div>
-			)}
-			{editMode && (
-				<div>
-					<CourseForm
-						setEditMode={setEditMode}
-						course={course}
-						admin={admin}
-					/>
-					<button onClick={() => setEditMode(false)}>
-						Cancel
-					</button>
-				</div>
-			)}
+			<div>
+				<Link to={`/courses/${course.id}`}>
+					<h3>{course.title}</h3>
+				</Link>
+				<p>{course.teacher}</p>
+				<p>{course.subject}</p>
+			</div>
 		</li>
 	);
 };
